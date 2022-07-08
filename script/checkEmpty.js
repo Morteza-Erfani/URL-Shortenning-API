89c18caca6abb7680ab739857b0bb4c01dac93a2
Morteza erfani <m.erfani74@gmail.com>
2022-07-08 16:28:41 +0430

    input and answers and features styled for 1440px

--- script/checkEmpty.js
+++ script/checkEmpty.js
@@ -2,8 +2,8 @@ const inputValue = document.querySelector("form input");
 document.querySelector("form button").addEventListener("click", (event) => {
   event.preventDefault();
   if (inputValue.value == "") {
-    inputValue.style.border = "red 2px solid";
-    inputValue.style.setProperty("--placeholder", "red");
+    inputValue.style.border = "var(--secondary-red) 2px solid";
+    inputValue.style.setProperty("--placeholder", "var(--secondary-red)");
     document.querySelector(".err").style.visibility = "visible";
   } else {
     document.querySelector(".err").style.visibility = "hidden";
