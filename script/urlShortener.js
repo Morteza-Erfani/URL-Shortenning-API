e197af6426aca5cc30439e6c582b5d4f6f475804
Morteza erfani <m.erfani74@gmail.com>
2022-07-10 15:56:47 +0430

    APIs added

--- script/urlShortener.js
+++ script/urlShortener.js
@@ -9,7 +9,7 @@ function shorter(longUrl) {
   const button = document.createElement("button");
 
   para.innerText = longUrl;
-  link.innerText = "shorter version";
+  // link.innerText = "shorter version";
   link.setAttribute("href", "#");
   link.setAttribute("id", `${id}`);
   button.innerText = "Copy";
@@ -23,6 +23,21 @@ function shorter(longUrl) {
   div.appendChild(button);
 
   answers.appendChild(div);
+
+  fetch("https://api.rebrandly.com/v1/links", {
+    method: "POST",
+    headers: {
+      apikey: "6a1da2968c1c45edb0aae769cff193d7",
+      workspace: "3425c75cbda546089b184d93f3b2af3a",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      destination: `${longUrl}`,
+      domain: { fullName: "rebrand.ly" },
+    }),
+  })
+    .then((Response) => Response.json())
+    .then((JSON) => link.innerText = JSON.shortUrl);
 
   id > 1 ? activeButton() : "";
 }
@@ -41,3 +56,5 @@ function copyButton(e) {
     e.srcElement.innerText = "Copied!";
   });
 }
+
+
